e020a59dab12203028eef64582f181391aa9247a
adermaulana <adermaulana15@gmail.com>
2023-06-04 04:48:35 +0800

    new home ui

--- database/migrations/2024_02_14_160253_create_obats_table.php
+++ database/migrations/2024_02_14_160253_create_obats_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('obats', function (Blueprint $table) {
             $table->id();
             $table->foreignId("unit_id");
+	    $table->foreignId("wadidaw_id");
             $table->string("nama_obat")->nullable();
             $table->string("stok")->nullable();
             $table->string("kadaluwarsa")->nullable();
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,61 +1,261 @@
 @extends('dashboard.layouts.login')
 
 @section('container')
-<main id="maino" class="maino container">
+    <div class="site-blocks-cover" style="background-image: url('/asset/images/hero_1.jpg');">
+        <div class="container">
+            <div class="row">
+            <div class="col-lg-7 mx-auto order-lg-2 align-self-center">
+                <div class="site-block-cover-content text-center">
+                <h2 class="sub-title">Effective Medicine, New Medicine Everyday</h2>
+                <h1>Welcome To Pharma</h1>
+                <p>
+                    <a href="#" class="btn btn-primary px-5 py-3">Shop Now</a>
+                </p>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
 
-@if ($message = Session::get('success'))
-<div class="alert alert-success alert-dismissible fade show col-lg-5" role="alert">
-        {{ session('success') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <div class="site-section">
+      <div class="container">
+        <div class="row align-items-stretch section-overlap">
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <div class="banner-wrap bg-primary h-100">
+              <a href="#" class="h-100">
+                <h5>Free <br> Shipping</h5>
+                <p>
+                  Amet sit amet dolor
+                  <strong>Lorem, ipsum dolor sit amet consectetur adipisicing.</strong>
+                </p>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <div class="banner-wrap h-100">
+              <a href="#" class="h-100">
+                <h5>Season <br> Sale 50% Off</h5>
+                <p>
+                  Amet sit amet dolor
+                  <strong>Lorem, ipsum dolor sit amet consectetur adipisicing.</strong>
+                </p>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <div class="banner-wrap bg-warning h-100">
+              <a href="#" class="h-100">
+                <h5>Buy <br> A Gift Card</h5>
+                <p>
+                  Amet sit amet dolor
+                  <strong>Lorem, ipsum dolor sit amet consectetur adipisicing.</strong>
+                </p>
+              </a>
+            </div>
+          </div>
+
+        </div>
       </div>
-@endif
+    </div>
 
-@if ($message = Session::get('error'))
-<div class="alert alert-danger alert-dismissible fade show col-lg-12" role="alert">
-        {{ session('error') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="title-section text-center col-12">
+            <h2 class="text-uppercase">Popular Products</h2>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-6 col-lg-4 text-center item mb-4">
+            <span class="tag">Sale</span>
+            <a href="shop-single.html"> <img src="/asset/images/product_01.png" alt="Image"></a>
+            <h3 class="text-dark"><a href="shop-single.html">Bioderma</a></h3>
+            <p class="price"><del>95.00</del> &mdash; $55.00</p>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col-12 text-center">
+            <a href="shop.html" class="btn btn-primary px-4 py-3">View All Products</a>
+          </div>
+        </div>
       </div>
-@endif
+    </div>
 
-<div class="pagetitle">
-  <h1>Daftar Obat Yang Tersedia</h1>
-</div><!-- End Page Title -->
-<table class="table table-bordered" id="datatable-noexport">
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="title-section text-center col-12">
+            <h2 class="text-uppercase">New Products</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 block-3 products-wrap">
+            <div class="nonloop-block-3 owl-carousel">
 
-		<thead>
-			<tr>
-				<th>No</th>
-				<th>Nama Obat</th>
-				<th>Gambar Obat</th>
-				<th>Harga Jual</th>
-				<th>Deskripsi Obat</th>
-				<th>Action</th>
-			</tr>
-		</thead>
-		<tbody>
-            @foreach($obat as $data)
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td> {{ $data->nama_obat }} </td>
-				@if($data->gambar == null )
-				<td><span class="badge bg-success">Tidak Ada Gambar</span> </td>
-				@else
-                <td><img width="100" src="{{ asset('storage/' . $data->gambar)  }}"></td>
-				@endif
-                <td> {{ $data->formatRupiah('harga_jual') }} </td>
-                <td> {{ $data->deskripsi_obat }} </td>
-				<td>
-					<div class="actions">
-						<a class="btn btn-success" href="{{ route('pelanggan.show', $data->id) }}">
-                      		<span data-feather="edit">Beli</span>
-						</a>
-					</div>
-				</td>
-            </tr>
-            @endforeach
-		</tbody>
-	</table>
+              <div class="text-center item mb-4">
+                <a href="shop-single.html"> <img src="/asset/images/product_03.png" alt="Image"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Umcka Cold Care</a></h3>
+                <p class="price">$120.00</p>
+              </div>
 
-</main>
+              <div class="text-center item mb-4">
+                <a href="shop-single.html"> <img src="/asset/images/product_01.png" alt="Image"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Umcka Cold Care</a></h3>
+                <p class="price">$120.00</p>
+              </div>
+
+              <div class="text-center item mb-4">
+                <a href="shop-single.html"> <img src="/asset/images/product_02.png" alt="Image"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Umcka Cold Care</a></h3>
+                <p class="price">$120.00</p>
+              </div>
+
+              <div class="text-center item mb-4">
+                <a href="shop-single.html"> <img src="/asset/images/product_04.png" alt="Image"></a>
+                <h3 class="text-dark"><a href="shop-single.html">Umcka Cold Care</a></h3>
+                <p class="price">$120.00</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="title-section text-center col-12">
+            <h2 class="text-uppercase">Testimonials</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 block-3 products-wrap">
+            <div class="nonloop-block-3 no-direction owl-carousel">
+        
+              <div class="testimony">
+                <blockquote>
+                  <img src="/asset/images/person_1.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
+                  <p>&ldquo;Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore obcaecati maiores voluptate aspernatur iusto eveniet, placeat ab quod tenetur ducimus. Minus ratione sit quaerat unde.&rdquo;</p>
+                </blockquote>
+
+                <p>&mdash; Kelly Holmes</p>
+              </div>
+        
+              <div class="testimony">
+                <blockquote>
+                  <img src="/asset/images/person_2.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
+                  <p>&ldquo;Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore
+                    obcaecati maiores voluptate aspernatur iusto eveniet, placeat ab quod tenetur ducimus. Minus ratione sit quaerat
+                    unde.&rdquo;</p>
+                </blockquote>
+              
+                <p>&mdash; Rebecca Morando</p>
+              </div>
+        
+              <div class="testimony">
+                <blockquote>
+                  <img src="/asset/images/person_3.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
+                  <p>&ldquo;Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore
+                    obcaecati maiores voluptate aspernatur iusto eveniet, placeat ab quod tenetur ducimus. Minus ratione sit quaerat
+                    unde.&rdquo;</p>
+                </blockquote>
+              
+                <p>&mdash; Lucas Gallone</p>
+              </div>
+        
+              <div class="testimony">
+                <blockquote>
+                  <img src="/asset/images/person_4.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
+                  <p>&ldquo;Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo omnis voluptatem consectetur quam tempore
+                    obcaecati maiores voluptate aspernatur iusto eveniet, placeat ab quod tenetur ducimus. Minus ratione sit quaerat
+                    unde.&rdquo;</p>
+                </blockquote>
+              
+                <p>&mdash; Andrew Neel</p>
+              </div>
+        
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section bg-secondary bg-image" style="background-image: url('/asset/images/bg_2.jpg');">
+      <div class="container">
+        <div class="row align-items-stretch">
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <a href="#" class="banner-1 h-100 d-flex" style="background-image: url('images/bg_1.jpg');">
+              <div class="banner-1-inner align-self-center">
+                <h2>Pharma Products</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae ex ad minus rem odio voluptatem.
+                </p>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-6 mb-5 mb-lg-0">
+            <a href="#" class="banner-1 h-100 d-flex" style="background-image: url('images/bg_2.jpg');">
+              <div class="banner-1-inner ml-auto  align-self-center">
+                <h2>Rated by Experts</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae ex ad minus rem odio voluptatem.
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
+
+            <div class="block-7">
+              <h3 class="footer-heading mb-4">About Us</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius quae reiciendis distinctio voluptates
+                sed dolorum excepturi iure eaque, aut unde.</p>
+            </div>
+
+          </div>
+          <div class="col-lg-3 mx-auto mb-5 mb-lg-0">
+            <h3 class="footer-heading mb-4">Quick Links</h3>
+            <ul class="list-unstyled">
+              <li><a href="#">Supplements</a></li>
+              <li><a href="#">Vitamins</a></li>
+              <li><a href="#">Diet &amp; Nutrition</a></li>
+              <li><a href="#">Tea &amp; Coffee</a></li>
+            </ul>
+          </div>
+
+          <div class="col-md-6 col-lg-3">
+            <div class="block-5 mb-5">
+              <h3 class="footer-heading mb-4">Contact Info</h3>
+              <ul class="list-unstyled">
+                <li class="address">203 Fake St. Mountain View, San Francisco, California, USA</li>
+                <li class="phone"><a href="tel://23923929210">+2 392 3929 210</a></li>
+                <li class="email">emailaddress@domain.com</li>
+              </ul>
+            </div>
+
+
+          </div>
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <p>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              Copyright &copy;
+              <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made
+              with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank"
+                class="text-primary">Colorlib</a>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </footer>
 
 @endsection
